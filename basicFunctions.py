76e5929da64ff6fa8b78fb0619e0d207247f5706
mariadop <m.h.dopierala@gmail.com>
2025-03-06 17:22:12 +0100

    created simple calculator in python

---
+++ basicFunctions.py
@@ -0,0 +1,12 @@
+def calculator(a, b, sign):
+    if sign == '+':
+        print(a + b)
+    elif sign == '-':
+        print(a - b)
+    elif sign == '*':
+        print(a * b)
+    else:
+        print(a / b)
+
+
+calculator(2, 3, '*')
