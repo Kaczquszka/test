8e597090d03bacfa45d8539f47706cfe6218f9dc
mariadop <m.h.dopierala@gmail.com>
2025-03-06 17:48:41 +0100

    wrote beginnings of prime number function

--- basicFunctions.py
+++ basicFunctions.py
@@ -1,3 +1,4 @@
+import math
 def calculator(a, b, sign):
     if sign == '+':
         print(a + b)
@@ -18,3 +19,17 @@ def calculator(a, b, sign):
 calculator(2, 3, '*')
 calculator(2, 2, '+')
 calculator(2, 0, '/')
+
+# has to be corrected because i is at some point a float
+
+
+def primeNumber(n):
+    isPrime = True
+    for i in range(math.sqrt(n)):
+        if n % i == 0:
+            isPrime = False
+    print(isPrime)
+  
+
+primeNumber(4)
+primeNumber(2)
